43bec4823b82c0f3c44ee96363688871e730c70c
Asteriskus <asteriskus04@gmail.com>
2020-11-28 12:30:26 +0700

    1

--- frontend/components/recent-contacts/index.jsx
+++ frontend/components/recent-contacts/index.jsx
@@ -10,12 +10,12 @@ const recentContacts = (props) =>
 
 const NamedPhoto = (item, i) =>
     <div className={css.photoWrapper} key={i}>
-        <img src={item.src}/>
+        <img src={item.photo} className={css.photo}/>
         <div>
             <h5>{item.name}</h5>
             <span>{item.desc}</span>
         </div>
-        <img src="/icons/send-mail.svg"></img>
+        <img src="/icons/send-mail.svg" className={css.mailIcon}/>
     </div>
 
 export default recentContacts;
--- frontend/pages/mocks/recent-contacts.js
+++ frontend/pages/mocks/recent-contacts.js
@@ -1,25 +1,25 @@
 export default [
     {
-        photo: '/vac1.png',
+        photo: '/photos/1.svg',
         name: 'Андрей Новожилов',
         desc: 'iOS-разработчик'
     },
     {
-        photo: '/vac2.png',
+        photo: '/photos/2.svg',
         name: 'Давид Вадимович …',
         desc: 'Android-разработчик'
     },
     {
-        photo: '/vac2.png',
+        photo: '/photos/3.svg',
         name: 'Илья Дмитриевич …',
         desc: 'Android-разработчик'
     },
     {
-        photo: '/vac2.png',
+        photo: '/photos/4.svg',
         name: 'Иван Барышев',
         desc: 'Дизайнер-интерфейсов'
     },{
-        photo: '/vac2.png',
+        photo: '/photos/5.svg',
         name: 'Марина Антоненко',
         desc: 'Маркетолог'
     },
