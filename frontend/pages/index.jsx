744dfdb3d83ffc4e3e0725483dfef40bc1e6d8e7
Yury Lunev <xalt@mail.ru>
2020-11-28 04:34:06 +0300

    Initial commit

--- frontend/pages/index.jsx
+++ frontend/pages/index.jsx
@@ -1,12 +1,12 @@
 import { useState, useEffect } from 'react';
 import axios from 'axios';
 
-const BACKEND = "http://choiceapp-backend:3000";
+const BFF = process.env.BFF || "http://choiceapp-bff:3000";
 
 const MainPage = () => {
     const [data, setData] = useState('Waiting...')
     useEffect(() => {
-        setTimeout(() => axios.post(BACKEND + '/api/v1/test_get', { data_id: 1 })
+        setTimeout(() => axios.post(BFF + '/api/v1/test_get', { data_id: 1 })
             .then(res => setData(JSON.stringify(res.data)))
             .catch(console.log.bind(null, 'Error on axios'))
             , 1500);
